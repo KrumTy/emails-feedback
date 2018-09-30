298e42622555d9fd0a9938c50e17890c8bdb599b
Krum Tyukenov <zarichepower@gmail.com>
2018-10-01 00:06:57 +0300

    add mongoose

--- server/index.js
+++ server/index.js
@@ -1,5 +1,15 @@
 const app = require('express')();
+const mongoose = require('mongoose');
+const { mongoURI } = require('./config/keys');
 const PORT = process.env.PORT || 5000;
+require('./models/User');
+
+mongoose.connect(
+  mongoURI,
+  {
+    useNewUrlParser: true
+  }
+);
 
 require('./services/passport');
 require('./routes/auth')(app);

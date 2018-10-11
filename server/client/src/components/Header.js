f7a6f49298daca4e48cdaf5aaf7e6219bf22b680
Krum Tyukenov <zarichepower@gmail.com>
2018-10-12 02:00:59 +0300

    add landing and rederects

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -28,9 +28,9 @@ class Header extends Component {
     return (
       <nav>
         <div className="nav-wrapper">
-          <a href="/" className="left brand-logo">
+          <Link to={this.props.auth ? '/surveys' : '/'} className="left brand-logo">
             Logo
-          </a>
+          </Link>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
             {this.renderContent()}
           </ul>
---
+++ server/client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Emails Feedback</h1>
+      Collect users feedback
+    </div>
+  );
+};
+
+export default Landing;

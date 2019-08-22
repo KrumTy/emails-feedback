e88b1f5e8668af1791123077abe72de213f01f77
Krum Tyukenov <zarichepower@gmail.com>
2019-08-22 18:04:56 +0300

    feat(survey): update feedback

--- server/routes/surveys.js
+++ server/routes/surveys.js
@@ -1,3 +1,6 @@
+const _ = require('lodash');
+const { Path } = require('path-parser');
+const { URL } = require('url');
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
@@ -7,8 +10,41 @@ const emailTemplates = require('../services/emailTemplates');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-  app.get('/api/surveys/thanks', (req, res) => {
+  app.get('/api/surveys/:surveyId/:choice', (req, res) => {
     res.send('Thanks for voting!');
+  });
+
+  app.post('/api/surveys/webhooks', (req, res) => {
+    _.chain(req.body)
+      .map(({ email, url }) => {
+        const { pathname } = new URL(url);
+        const p = new Path('/api/surveys/:surveyId/:choice');
+        const match = p.test(pathname);
+
+        return match && { email, ...match };
+      })
+      .compact()
+      .uniqBy('email', 'surveyId')
+      .each(({ email, surveyId, choice }) => {
+        Survey.updateOne(
+          {
+            _id: surveyId,
+            recipients: {
+              $elemMatch: {
+                email,
+                hasResponded: false
+              }
+            }
+          },
+          {
+            $inc: { [choice]: 1 },
+            $set: { 'recipients.$.hasResponded': true, lastRespondedOn: new Date() }
+          }
+        ).exec();
+      })
+      .value();
+
+    res.send({});
   });
 
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
---
+++ server/sendgrid_webhook.js
@@ -0,0 +1,5 @@
+var localtunnel = require('localtunnel');
+
+localtunnel(5000, { subdomain: 'hufdsjfoiunhxvbjhc7i7yu' }, (err, tunnel) => {
+  console.log('LT running');
+});
